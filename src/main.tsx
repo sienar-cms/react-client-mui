a0ecac37db5bb825130c92fff958a9b27a14404d
christianlevesque <christian@levesque.dev>
2024-10-01 20:14:27 -0500

    set up authentication store in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,6 @@
-import { createApp } from '@/react-utils';
+import { createApp, addReducer, authReducer, AUTH_NAME } from '@/react-utils';
+
+addReducer(AUTH_NAME, authReducer);
 
 // Build and run React app
 createApp();
