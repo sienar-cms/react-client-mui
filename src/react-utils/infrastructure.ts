85032a5115a88e85f7392d70ad506d3d9e7ac5f4
christianlevesque <christian@levesque.dev>
2024-10-05 19:20:34 -0500

    added system for setting URLs in a container and retrieving them, allowing a sort of primitive named route system

--- src/react-utils/infrastructure.ts
+++ src/react-utils/infrastructure.ts
@@ -319,3 +319,39 @@ export enum MenuPriority {
 }
 
 // endregion
+
+// region URLs
+
+const urls: Record<string, string> = {};
+
+/**
+ * Sets a URL in the URL container
+ *
+ * @param name The name of the URL to set
+ * @param value The URL value
+ */
+export function setUrl(name: string, value: string) {
+	urls[name] = value;
+}
+
+/**
+ * Gets a URL from the URL container
+ *
+ * @param name The name of the URL to get
+ * @param fallback A fallback value to use if the URL is not found
+ */
+export function getUrl(name: string, fallback?: string|undefined): string {
+	if (!urls[name] && !fallback) throw new Error(`Unable to locate URL named ${name}`);
+
+	return urls[name] ?? fallback;
+}
+
+const sienarUrls = {
+	DASHBOARD: 'dashboard',
+	LOGIN: 'login',
+	UNAUTHORIZED: 'unauthorized'
+};
+
+export const SIENAR_URLS = Object.freeze(sienarUrls);
+
+// endregion
