60d1f61eec8170ebae0d5c39bafb982144c4d14e
christianlevesque <christian@levesque.dev>
2024-10-20 09:28:22 -0500

    added <Narrow> wrapper to registration and register successful views

--- src/views/account/register/Index.tsx
+++ src/views/account/register/Index.tsx
@@ -1,5 +1,5 @@
 ﻿import { useState } from 'react';
-import { Form } from '@/react-ui';
+import { Form, Narrow } from '@/react-ui';
 import { validators, SIENAR_URLS, useNavigate } from '@/react-utils';
 
 export default function Index() {
@@ -8,74 +8,76 @@ export default function Index() {
 	const [ email, setEmail ] = useState('');
 
 	return (
-		<Form.Form
-			id='register'
-			title='Register'
-			method='POST'
-			action='/api/account'
-			onSuccess={(result: boolean) => {
-				if (result) {
-					navigate(
-						SIENAR_URLS.REGISTER_SUCCESSFUL,
-						{
-							username,
-							email
-						}
-					);
-				}
-			}}
-		>
-			<Form.Textbox
-				name='username'
-				displayName='Username'
-				value={username}
-				onChange={setUsername}
-				validators={[
-					validators.required(),
-					validators.minLength(6),
-					validators.maxLength(32)
-				]}
-			/>
-			<Form.Textbox
-				name='email'
-				displayName='Email address'
-				type='email'
-				value={email}
-				onChange={setEmail}
-				validators={[
-					validators.required(),
-					validators.isEmail()
-				]}
-			/>
-			<Form.Textbox
-				name='password'
-				displayName='Password'
-				type='password'
-				validators={[
-					validators.minLength(8),
-					validators.maxLength(64),
-					validators.containsNumber(),
-					validators.containsLower(),
-					validators.containsUpper(),
-					validators.containsSpecialCharacter()
-				]}
-			/>
-			<Form.Textbox
-				name='confirmPassword'
-				displayName='Confirm password'
-				type='password'
-				validators={[
-					validators.matches('Password')
-				]}
-			/>
-			<Form.Checkbox
-				name='acceptTos'
-				displayName='Accept TOS'
-				validators={[
-					validators.required()
-				]}
-				hideNonErrors
-			/>
-		</Form.Form>
+		<Narrow>
+			<Form.Form
+				id='register'
+				title='Register'
+				method='POST'
+				action='/api/account'
+				onSuccess={(result: boolean) => {
+					if (result) {
+						navigate(
+							SIENAR_URLS.REGISTER_SUCCESSFUL,
+							{
+								username,
+								email
+							}
+						);
+					}
+				}}
+			>
+				<Form.Textbox
+					name='username'
+					displayName='Username'
+					value={username}
+					onChange={setUsername}
+					validators={[
+						validators.required(),
+						validators.minLength(6),
+						validators.maxLength(32)
+					]}
+				/>
+				<Form.Textbox
+					name='email'
+					displayName='Email address'
+					type='email'
+					value={email}
+					onChange={setEmail}
+					validators={[
+						validators.required(),
+						validators.isEmail()
+					]}
+				/>
+				<Form.Textbox
+					name='password'
+					displayName='Password'
+					type='password'
+					validators={[
+						validators.minLength(8),
+						validators.maxLength(64),
+						validators.containsNumber(),
+						validators.containsLower(),
+						validators.containsUpper(),
+						validators.containsSpecialCharacter()
+					]}
+				/>
+				<Form.Textbox
+					name='confirmPassword'
+					displayName='Confirm password'
+					type='password'
+					validators={[
+						validators.matches('Password')
+					]}
+				/>
+				<Form.Checkbox
+					name='acceptTos'
+					displayName='Accept TOS'
+					validators={[
+						validators.required()
+					]}
+					hideNonErrors
+				/>
+			</Form.Form>
+		</Narrow>
 	);
 }
--- src/views/account/register/Successful.tsx
+++ src/views/account/register/Successful.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from 'react-router-dom';
 import { Typography } from '@mui/material';
+import { Narrow } from '@/react-ui';
 
 export default function Successful() {
 	const [ params ] = useSearchParams();
@@ -7,11 +8,11 @@ export default function Successful() {
 	const email = params.get('email');
 
 	return (
-		<>
+		<Narrow>
 			<Typography typography='h1'>Registered successfully</Typography>
 			<Typography>
 				Thank you for registering, {username}! A welcome email has been sent to {email}. Please click the verification link in the welcome email to verify your account.
 			</Typography>
-		</>
+		</Narrow>
 	);
 }
