260b079c22de8e8a46f01fcfb7b2c5e41a27ee3f
christianlevesque <christian@levesque.dev>
2024-10-23 10:54:40 -0500

    renamed registerService() to provide(); rewrote inject() and added overloads to allow for optional requests to the DI container

--- src/react-utils/infrastructure/di.ts
+++ src/react-utils/infrastructure/di.ts
@@ -1,39 +1,52 @@
-﻿// This DI container is loosely inspired by VueJS' <code>provide</code>/<code>inject</code> functionality. For the most part, I just borrowed the *idea* to create this (much simpler) DI container; however, the interface <code>InjectionConstraint<T></code> and type <code>InjectionKey<T></code> are directly copied from the VueJS Inject API at https://github.com/vuejs/core/blob/v3.5.12/packages/runtime-core/src/apiInject.ts#L7-L9. The purpose of the copying was to facilitate explicit typing of injected services when using Typescript. The rest of this DI container is custom code that works fundamentally differently from the VueJS Inject API. VueJS is released under the MIT License.
+﻿// This DI container is loosely inspired by VueJS' <code>provide</code>/<code>inject</code> functionality. For the most part, I just borrowed the *idea* to create this (much simpler) DI container; however, the function names are the same (why waste a good function name?), and the interface <code>InjectionConstraint<T></code> and type <code>InjectionKey<T></code> are directly copied from the VueJS Inject API at https://github.com/vuejs/core/blob/v3.5.12/packages/runtime-core/src/apiInject.ts#L7-L9. The purpose of the copying was to facilitate explicit typing of injected values when using Typescript. The rest of this DI container is custom code that works fundamentally differently from the VueJS Inject API. VueJS is released under the MIT License.
 
-// @ts-ignore: The type in InjectionConstraint is unused, but it is required to be here because it is used when strongly typing services returned using the <code>inject</code> function below.
+// @ts-ignore: The type in InjectionConstraint is unused, but it is required to be here because it is used when explicitly typing values returned using the <code>inject</code> function below.
 interface InjectionConstraint<T> {}
 export type InjectionKey<T> = symbol & InjectionConstraint<T>;
 
 const di: Record<InjectionKey<any>, any> = {};
 
 /**
- * Registers a service in the Sienar DI container
+ * Registers a value in the Sienar DI container
  *
- * @param key The unique key that identifies the service in the container
- * @param service The service to register
- * @param override Whether to override an existing service if one is already registered
+ * @param key The unique key that identifies the value in the container
+ * @param value The value to register
+ * @param override Whether to override an existing value if one is already registered
  */
-export function registerService<T>(
+export function provide<T>(
 	key: InjectionKey<T>,
-	service: T,
+	value: T,
 	override: boolean = true
 ) {
-	if (override) di[key] = service;
-	else di[key] ??= service;
+	if (override) di[key] = value;
+	else di[key] ??= value;
 }
 
 /**
- * Injects a service from the Sienar DI container
+ * Injects a value from the Sienar DI container
  *
- * @param key The unique key that identifies the service in the container
- * @returns The service if it exists
- *
- * @throws Error If no service is registered with the given key
+ * @param key The unique key that identifies the value in the container
+ * @param required Whether to fail if the value is not found
+ * @returns The value if it exists
  */
-export function inject<T>(key: InjectionKey<T>): T {
-	if (!di[key]) throw new Error(`Unable to locate service with key ${String(key)}`);
+export function inject<T>(key: InjectionKey<T>, required?: false): T|undefined;
 
-	return di[key];
+/**
+ * Injects a required value from the Sienar DI container
+ *
+ * @param key The unique key that identifies the value in the container
+ * @param required Whether to fail if the value is not found
+ * @returns The value
+ *
+ * @throws Error If no value is registered with the given key
+ */
+export function inject<T>(key: InjectionKey<T>, required: true): T;
+
+export function inject<T>(key: InjectionKey<T>, required: boolean = false): T {
+	const value = di[key];
+	if (!value && required) throw new Error('Unable to locate value with the provided key');
+
+	return value;
 }
 
 export type Logger = {
