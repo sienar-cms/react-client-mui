e339e6aa4488f0aef900787070bd6928141e56a8
christianlevesque <christian@levesque.dev>
2024-10-27 14:04:01 -0500

    added way to set an error view in DI

--- src/react-utils/router.ts
+++ src/react-utils/router.ts
@@ -1,8 +1,13 @@
 ﻿import type { ReactNode } from 'react'
 import type { RouteObject } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
+import { inject } from '@/react-utils/infrastructure/di';
+
+import type { InjectionKey } from '@/react-utils/infrastructure/di';
 
 const routes = new Map<ReactNode, RouteObject[]>();
+
+export const ERROR_VIEW = Symbol() as InjectionKey<ReactNode>;
 
 export function registerRoutes(layout: ReactNode, ...items: RouteObject[]): void {
 	if (!routes.has(layout)) {
@@ -16,11 +21,13 @@ export function registerRoutes(layout: ReactNode, ...items: RouteObject[]): void
 
 export function createRouter() {
 	const layoutRoutes: RouteObject[] = [];
+	const errorComponent = inject(ERROR_VIEW, true);
 
 	for (let [layout, childRoutes] of routes) {
 		layoutRoutes.push({
 			path: '',
 			element: layout,
+			errorElement: errorComponent,
 			children: childRoutes
 		});
 	}
