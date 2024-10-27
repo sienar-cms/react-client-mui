384f3df650a4141ad015d88628bfdf0daeb894ec
christianlevesque <christian@levesque.dev>
2024-10-27 10:26:14 -0500

    updated snackbar appearance

--- src/react-ui/Snackbar.tsx
+++ src/react-ui/Snackbar.tsx
@@ -1,4 +1,6 @@
-﻿import { SnackbarProvider } from 'notistack';
+﻿import { SnackbarProvider, closeSnackbar } from 'notistack';
+import { IconButton } from '@mui/material';
+import { Cancel, CheckCircle, Error, Help, Warning } from '@mui/icons-material';
 import type { PropsWithChildren } from 'react';
 
 export default function Snackbar({ children }: PropsWithChildren) {
@@ -9,6 +11,20 @@ export default function Snackbar({ children }: PropsWithChildren) {
 				vertical: 'top'
 			}}
 			maxSnack={5}
+			iconVariant={{
+				success: <CheckCircle sx={{ mr: 1 }}/>,
+				warning: <Warning sx={{ mr: 1 }}/>,
+				error: <Error sx={{ mr: 1 }}/>,
+				info: <Help sx={{ mr: 1 }}/>
+			}}
+			action={id => (
+				<IconButton
+					color='inherit'
+					onClick={() => closeSnackbar(id)}
+				>
+					<Cancel/>
+				</IconButton>
+			)}
 		>
 			{children}
 		</SnackbarProvider>
