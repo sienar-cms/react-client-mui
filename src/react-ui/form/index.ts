0352f8c5efcaa7016edaba709b2bed4c1395ddd1
Christian LeVesque <christian@levesque.dev>
2024-11-02 17:40:52 -0500

    added new <Switch> managed component to handle toggling of a single value and immediately responding to the toggle

--- src/react-ui/form/index.ts
+++ src/react-ui/form/index.ts
@@ -6,5 +6,7 @@ export type * from './Textbox.tsx';
 export { default as Textbox } from './Textbox.tsx';
 export type * from './Checkbox.tsx';
 export { default as Checkbox } from './Checkbox.tsx';
+export type * from './Switch.tsx';
+export { default as Switch } from './Switch.tsx';
 
 export type * from './shared.ts';
