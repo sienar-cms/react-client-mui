5bf7fc85cd56fb14234f37582e0d300ec100f9e7
Christian LeVesque <christian@levesque.dev>
2024-11-02 16:44:46 -0500

    added a new sleep() utility that simulates Thread.Sleep(ms)

--- src/react-utils/utils.ts
+++ src/react-utils/utils.ts
@@ -67,3 +67,12 @@ export async function sendRequest<T>(
 
 	return result.result;
 }
+
+/**
+ * Simulates waiting for a specified amount of time. Probably only useful during development.
+ *
+ * @param time The amount of time to "sleep", in milliseconds
+ */
+export function sleep(time: number): Promise<void> {
+	return new Promise(resolve => setTimeout(resolve, time));
+}
