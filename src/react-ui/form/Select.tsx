5dd64bff87cfa8fa56a056f38b71547ae32af9a1
Christian LeVesque <christian@levesque.dev>
2024-11-02 20:28:37 -0500

    added <Select> component based on MUI's <NativeSelect>

---
+++ src/react-ui/form/Select.tsx
@@ -0,0 +1,88 @@
+import { useEffect, useId, useRef } from 'react';
+import { FormControl, InputLabel, NativeSelect as MaterialSelect, OutlinedInput } from '@mui/material';
+import ValidationList from './ValidationList.tsx';
+import { useFormFieldValidation, useRerender } from '@/react-utils';
+
+import type { ReactNode } from 'react';
+import type { FormInputProps } from './shared.ts';
+
+export type SelectProps = FormInputProps<string> & {
+	label?: ReactNode,
+	fullWidth?: boolean
+}
+
+export default function Select(props: SelectProps) {
+	const {
+		children,
+		label,
+		name,
+		displayName,
+		hideNonErrors,
+		hideValidationIfValid = true,
+		allValidMessage,
+		validators = [],
+		onChange,
+		fullWidth = true
+	} = props;
+
+	const inputId = useId();
+	const currentValue = useRef('');
+	const fieldRef = useRef<HTMLSelectElement>(null);
+	const rerender = useRerender();
+	const [ validations, interact ] = useFormFieldValidation(name, displayName, currentValue, validators);
+
+	const handleChange = async (e: Event) => {
+		console.log(e.target);
+		const target = e.target as HTMLSelectElement;
+		const newValue = target.value;
+		if (currentValue.current !== newValue) {
+			currentValue.current = newValue;
+			interact();
+			await onChange?.(newValue);
+			rerender();
+		}
+	}
+
+	useEffect(() => {
+		const ref = fieldRef.current!;
+		ref.addEventListener('change', handleChange);
+		return () => ref.removeEventListener('change', handleChange);
+	});
+
+	return (
+		<>
+			<FormControl fullWidth={fullWidth}>
+				<InputLabel
+					variant='outlined'
+					htmlFor={inputId}
+					shrink
+				>
+					{label ?? displayName}
+				</InputLabel>
+
+				<MaterialSelect
+					inputRef={fieldRef}
+					defaultValue={''}
+					name={name}
+					error={validations.filter(v => !v.valid).length > 0}
+					variant={'outlined'}
+					input={<OutlinedInput label={label ?? displayName} notched/>}
+					inputProps={{
+						name: name,
+						id: inputId
+					}}
+					sx={{ mb: 1 }}
+				>
+					{children}
+				</MaterialSelect>
+			</FormControl>
+
+			<ValidationList
+				validations={validations}
+				hideNonErrors={hideNonErrors}
+				hideIfAllValid={hideValidationIfValid}
+				allValidMessage={allValidMessage}
+			/>
+		</>
+	);
+}
--- src/react-ui/form/index.ts
+++ src/react-ui/form/index.ts
@@ -6,6 +6,8 @@ export type * from './Textbox.tsx';
 export { default as Textbox } from './Textbox.tsx';
 export type * from './Checkbox.tsx';
 export { default as Checkbox } from './Checkbox.tsx';
+export type * from './Select.tsx';
+export { default as Select } from './Select.tsx';
 export type * from './Switch.tsx';
 export { default as Switch } from './Switch.tsx';
 
