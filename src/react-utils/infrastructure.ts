766d4520d75702a034a268b24acbc0fec0ea5856
Christian LeVesque <christian@levesque.dev>
2024-11-06 12:28:33 -0600

    added useDocumentTitle hook for setting the document title on page render

--- src/react-utils/infrastructure.ts
+++ src/react-utils/infrastructure.ts
@@ -1,12 +1,35 @@
-﻿import { createContext, useContext } from 'react';
+﻿import { createContext, useContext, useEffect } from 'react';
+import { inject } from '@/react-utils/di.ts';
 import type { InjectionKey } from '@/react-utils/di.ts';
 import type { MenuLinkProvider } from '@/react-utils/menus.ts';
+
+export const DOCUMENT_TITLE_SUFFIX = Symbol() as InjectionKey<string>;
 
 export const infrastructureContext = createContext<InfrastructureContext>({
 	activeMenu: Symbol(),
 	setActiveMenu: () => {}
 });
 export const useInfrastructureContext = () => useContext(infrastructureContext);
+
+/**
+ * Sets the intended document title on initial render
+ *
+ * @param title The title to use
+ * @param ignoreSuffix Whether to ignore the suffix supplied from DI, even if it exists
+ */
+export function useDocumentTitle(title: string, ignoreSuffix: boolean = false) {
+	useEffect(() => {
+		let calculatedTitle = title;
+		if (!ignoreSuffix) {
+			const suffix = inject(DOCUMENT_TITLE_SUFFIX, true);
+			if (suffix) {
+				calculatedTitle = `${title} ${suffix}`;
+			}
+		}
+
+		document.title = calculatedTitle;
+	}, []);
+}
 
 /**
  * The app's infrastructure-related state
