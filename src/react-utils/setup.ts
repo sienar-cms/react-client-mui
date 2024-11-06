1c51a3d0cd209809a17eef23b95d4892fa1759c2
Christian LeVesque <christian@levesque.dev>
2024-11-06 11:47:12 -0600

    removed StrictMode as the root provider

--- src/react-utils/setup.ts
+++ src/react-utils/setup.ts
@@ -2,13 +2,11 @@
 import { API_CALLER } from '@/react-utils/http.ts';
 import { sendRequest } from '@/react-utils/utils.ts';
 import { registerProvider } from '@/react-utils/providers.tsx';
-import { StrictMode } from 'react';
 import AuthProvider from '@/react-utils/components/AuthProvider.tsx';
 import InfrastructureProvider from '@/react-utils/components/InfrastructureProvider.tsx';
 
 export default function () {
 	provide(API_CALLER, sendRequest);
-	registerProvider(StrictMode);
 	registerProvider(AuthProvider);
 	registerProvider(InfrastructureProvider);
 }
