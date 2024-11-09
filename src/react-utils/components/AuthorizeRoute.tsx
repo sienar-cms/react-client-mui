20a5a752de4b38993a264df8645afc3b02ce9b4b
Christian LeVesque <christian@levesque.dev>
2024-11-09 13:03:34 -0600

    updated AuthorizeRoute to use injection keys pointing to partials in DI instead of displaying a message and redirecting to various pages

--- src/react-utils/components/AuthorizeRoute.tsx
+++ src/react-utils/components/AuthorizeRoute.tsx
@@ -1,56 +1,48 @@
-﻿import { useEffect, useState } from 'react';
-import { StatusPage } from '@/react-ui';
-import { useAuthorized, useAuthContext, useNavigate, inject } from '@/react-utils';
-import { LOGIN_ROUTE, UNAUTHORIZED_ROUTE } from '@identity/urls.ts';
+﻿import { AUTH_MISSING_ROLES_PARTIAL, AUTH_MUST_BE_LOGGED_IN_PARTIAL, AUTH_MUST_BE_LOGGED_OUT_PARTIAL, useAuthorized, useAuthContext } from '@/react-utils/auth.ts';
+import { inject } from '@/react-utils/di.ts';
 import type { AuthorizeProps } from '@/react-utils/components/Authorize.tsx';
-import type { PropsWithChildren } from 'react';
+import type { InjectionKey } from '@/react-utils/di.ts';
+import type { PropsWithChildren, ReactNode } from 'react';
 
 export type AuthorizeRouteProps = Pick<AuthorizeProps, 'any'|'roles'> & {
 	mustBeLoggedOut?: boolean
+	missingRolesKey?: InjectionKey<ReactNode>
+	mustBeLoggedInKey?: InjectionKey<ReactNode>
+	mustBeLoggedOutKey?: InjectionKey<ReactNode>
 }
 
 export default function AuthorizeRoute(props: PropsWithChildren<AuthorizeRouteProps>) {
-	const { roles, any, mustBeLoggedOut, children } = props;
-	const navigate = useNavigate();
+	const {
+		roles,
+		any,
+		mustBeLoggedOut,
+		children,
+		missingRolesKey = AUTH_MISSING_ROLES_PARTIAL,
+		mustBeLoggedInKey = AUTH_MUST_BE_LOGGED_IN_PARTIAL,
+		mustBeLoggedOutKey = AUTH_MUST_BE_LOGGED_OUT_PARTIAL
+	} = props;
+
 	const authContext = useAuthContext();
 	const { isLoggedIn } = authContext;
 	const isAuthorizedToApp = useAuthorized(roles, any);
-	const [ secondsUntilRedirect, setSecondsUntilRedirect ] = useState(5);
 	const isAuthorizedToViewPage = mustBeLoggedOut ? !isLoggedIn : isAuthorizedToApp;
 
-	useEffect(() => {
-		if (isAuthorizedToViewPage) return;
+	let content: ReactNode;
 
-		if (secondsUntilRedirect > 0) {
-			const timeoutId = setTimeout(() => setSecondsUntilRedirect(secondsUntilRedirect - 1), 1000);
-			return () => clearTimeout(timeoutId);
-		}
+	switch (true) {
+		case isAuthorizedToViewPage:
+			content = children;
+			break;
+		case mustBeLoggedOut:
+			content = inject(mustBeLoggedOutKey);
+			break;
+		case isLoggedIn:
+			content = inject(missingRolesKey);
+			break;
+		default:
+			content = inject(mustBeLoggedInKey);
+			break;
+	}
 
-		if (mustBeLoggedOut) {
-			navigate(UNAUTHORIZED_ROUTE);
-		} else {
-			let route = isLoggedIn
-				? inject(UNAUTHORIZED_ROUTE)
-				: inject(LOGIN_ROUTE);
-
-			if (window.location.pathname === inject(LOGIN_ROUTE)) {
-				return;
-			}
-
-			navigate(`${route}?returnUrl=${window.location.pathname}`);
-		}
-
-	}, [isAuthorizedToApp, secondsUntilRedirect]);
-
-	if (isAuthorizedToViewPage) return <>{children}</>;
-
-	let pageTitle: string;
-	let pageContent: string;
-	let pageName: string;
-
-	return (
-		<StatusPage title={pageTitle}>
-			{pageContent} You will be redirected to the {pageName} page in {secondsUntilRedirect} second{secondsUntilRedirect !== 1 && 's'}.
-		</StatusPage>
-	);
+	return <>{content}</>;
 }
