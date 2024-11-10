49e97c218ebfd87f766a037e718c7f2bfbf1bc83
Christian LeVesque <christian@levesque.dev>
2024-11-10 17:51:11 -0600

    added link to the login page with the returnUrl query string included

--- src/partials/MustBeLoggedIn.tsx
+++ src/partials/MustBeLoggedIn.tsx
@@ -1,9 +1,12 @@
 import { StatusPage } from '@/react-ui';
+import { Link } from 'react-router-dom';
+import { inject } from '@/react-utils';
+import { LOGIN_ROUTE } from '@identity/urls.ts';
 
 export default function MustBeLoggedIn() {
 	return (
 		<StatusPage title='You must be logged in'>
-			You must be logged in to view this page.
+			You must be logged in to view this page. <Link to={`${inject(LOGIN_ROUTE)}?returnUrl=${window.location.pathname}`}>Go to login page</Link>
 		</StatusPage>
 	);
 }
