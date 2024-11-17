b19c4f98e779971614bd7836113787a2e9d9c73c
Christian LeVesque <christian@levesque.dev>
2024-11-16 19:21:24 -0600

    removed @sienar/react-utils source files

--- src/react-utils/setup.ts
+++
@@ -1,8 +0,0 @@
-﻿import { registerProvider } from '@/react-utils/providers.tsx';
-import AuthProvider from '@/react-utils/components/AuthProvider.tsx';
-import InfrastructureProvider from '@/react-utils/components/InfrastructureProvider.tsx';
-
-export default function () {
-	registerProvider(AuthProvider);
-	registerProvider(InfrastructureProvider);
-}
